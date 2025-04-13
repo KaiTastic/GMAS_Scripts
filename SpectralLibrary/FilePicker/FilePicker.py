49e5c944e7ca4f81b05e877b835b6942412fb4b8
Kai Cao <caokai_cgs@163.com>
2025-04-13 16:10:22 +0300

    - Added photo picker moudle.

---
+++ SpectralLibrary/FilePicker/FilePicker.py
@@ -0,0 +1,91 @@
+Surface_not_shown = ['44234B241_FSP02.jpg', '41221B086-1-LF1.jpg', '41221B086-1-LF2.jpg', '41221B104-1-LF3.jpg', '41221B104-1-LF4.jpg', '41221B118-1-LW4.jpg', '41221B166-2-LF1.jpg', '41221B166-2-LF2.jpg', '41221B172-1-LW4.jpg', '41221B173-2-LF1.jpg', '41221B173-2-LF2.jpg', '41221B187-1-LW3.jpg', '41221B187-2-LF1.jpg', '41221B187-2-LF2.jpg', '41221B187-2-LF3.jpg', '42224B077-1-LF4.jpg', '42224B082-1-LF3.jpg', '42224B088-1-LF2.jpg', '45234A003-1-LF4.jpg', '45234B025-1-LF1.jpg', '45234B025-1-LF2.jpg', '45234B025-1-LW1.jpg', '45234B027-1-LF3.jpg', '45234B027-1-LF4.jpg', '45234B030-1-LF2.jpg', '45234B030-1-LF3.jpg', '45234B031-1-LF1.jpg', '45234B031-1-LF2.jpg', '45234B031-1-LF3.jpg', '45234B031-1-LF4.jpg', '45234B038-1-LF1.jpg', '45234B038-1-LF2.jpg', '45234B050-1-LF1.jpg', '45234B050-1-LF2.jpg', '45234B051-1-LF1.jpg', '45234B051-1-LF2.jpg', '45234B051-1-LF3.jpg', '45234B065-1-LF2.jpg', '45234B065-1-LF3.jpg', '45234B065-1-LW2.jpg', '45234B070-1-LW1.jpg', '45234B070-1-LW2.jpg', '45234B085-1-LF4.jpg', '45234B107-1-LW1.jpg', '45234B107-1-LW2.jpg', '45234B132-1-LF3.jpg', '45234B132-1-LF4.jpg', '45234B138-1-LF3.jpg', '45234B140-1-LF3.jpg', '45234B140-1-LF4.jpg', '45234B141-1-LF3.jpg', '45234B141-1-LF4.jpg', '45234B142-1-LF1.jpg', '45234B142-1-LF2.jpg', '45234B142-1-LF3.jpg', '45234B142-1-LW3.jpg', '45234B221-1-LF1.jpg', '45234B221-1-LF2.jpg', '45234B226-1-LF2.jpg', '45234B227-1-LW4.jpg', '45234B228-1-LF1.jpg', '45234B228-1-LF2.jpg', '45234B228-1-LF4.jpg', '45234B228-1-Lw3.jpg', '45234B228-1-LW4.jpg']
+
+Photo_blurry = ['45234A093-1-LF3.jpg', '45234A093-1-LF4.jpg', '45234A097-1-LF1.jpg', '45234A097-1-LF2.jpg', '45234A097-1-LF3.jpg', '45234A100-1-LF3.jpg', '45234A113-1-LF2.jpg', '45234A116-1-LF1.jpg', '45234A116-1-LF4.jpg', '45234A116-1-LF3.jpg', '45234A117-1-LF4.jpg', '45234A118-1-LF1.jpg', '45234A118-1-LF4.jpg', '45234A122-1-LF1.jpg', '45234A122-1-LF4.jpg', '45234A128-1-LF4.jpg', '45234A129-1-LF3.jpg', '45234A131-1-LF1.jpg', '45234A183-1-LF3.jpg', '45234A183-1-LF1.jpg', '45234A183-1-LF4.jpg', '45234A184-1-LF4.jpg', '45234A184-1-LF2.jpg', '45234A184-1-LF4.jpg', '45234A184-1-LF3.jpg', '45234A185-1-LF4.jpg', '45234A185-1-LF2.jpg', '45234A187-1-LF3.jpg', '45234A189-1-LF1.jpg', '45234A189-1-LF2.jpg', '45234A191-1-LF4.jpg', '45234A191-1-LF2.jpg', '45234A191-1-LF3.jpg', '45234A191-1-LF4.jpg', '45234A210-1-LF2.jpg', '45234A210-1-LF4.jpg', '45234A232-1-LF1.jpg', '45234A232-1-LF3.jpg', '45234A232-1-LF2.jpg', '45234A232-1-LF4.jpg', '45234A276-1-LF3.jpg', '45234A277-1-LF3.jpg', '45234A277-1-LF4.jpg', '45234A278-1-LF1.jpg', '45234A278-1-LF2.jpg', '45234A278-1-LF3.jpg', '45234A278-1-LF4.jpg', '45234A284-1-LF2.jpg', '45234A312-1-LF3.jpg', '45234A368-1-LF2.jpg', '45234B025-1-LF4.jpg', '45234B027-1-LF2.jpg', '45234B027-1-LF3.jpg', '45234B029-1-LF1.jpg', '45234B029-1-LF3.jpg', '45234B051-1-LF4.jpg', '45234B285-1-LF3.jpg', '45234B285-1-LF4.jpg', '45234B409-1-LF4.jpg', '45234B413-1-LF1.jpg', '45234B413-1-LF2.jpg', '45234B418-1-LF1.jpg', '45234B418-1-LF4.jpg', '41221A052-1-LW1.jpg', '41221A080-2-LW4.jpg', '41221B010-2-LW3.jpg', '41221B048-1-LW2.jpg', '41221B048-1-LF2.jpg', '41221B172-1-LW1.jpg', '41221B187-1-LF2.jpg', '42224A028-1-LW1.jpg', '45234A001-1-LW4.jpg', '45234B050-1-LF3.jpg', '45234B050-1-LW3.jpg', '45234B050-1-LW4.jpg', '45234B051-1-LW4.jpg', '45234B052-1-LF3.jpg']
+
+Surface_not_clean = ['41221A021-1-LW4.jpg', '41221A022-1-LW3.jpg', '41221A023-1-LW2.jpg', '41221A036-1-LW2.jpg', '41221A052-1-LW1.jpg', '41221A052-1-LW3.jpg', '41221A054-1-LW2.jpg', '41221A054-2-LW4.jpg', '41221A055-1-LW1.jpg', '41221A071-1-LW3.jpg', '41221A073-1-LW2.jpg', '41221A073-1-LW3.jpg', '41221A080-1-LW5.jpg', '41221A104-1-LW1.jpg', '41221A104-1-LW2.jpg', '41221A117-1-LW3.jpg', '41221A117-1-LW4.jpg', '41221A128-1-LW1.jpg', '41221A128-1-LW2.jpg', '41221B005-1-LW1.jpg', '41221B005-2-LW1.jpg', '41221B014-1-LW3.jpg', '41221B048-2-LW3.jpg', '41221B048-2-LW4.jpg', '41221B073-2-LW2.jpg', '41221B079-2-LW2.jpg', '41221B086-1-LW3.jpg', '41221B086-1-LW1.jpg', '41221B086-1-LW2.jpg', '41221B104-2-LF2.jpg', '41221B118-2-LW2.jpg', '41221B118-1-LW4.jpg', '41221B172-1-LW3.jpg', '42224A004-1-LF3.jpg', '42224A006-1-LW4.jpg', '42224A019-1-LW4.jpg', '42224A021-1-LW4.jpg', '42224A034-1-LW4.jpg', '42224A035-1-LW3.jpg', '42224A035-1-LW4.jpg', '42224A050-1-LW1.jpg', '42224A054-2-LW4.jpg', '42224B003-1-LW5.jpg', '42224B009-1-LW4.jpg', '42224B014-1-LW3.jpg', '42224B019-1-LW4.jpg', '42224B045-1-LW4.jpg', '42224B050-1-LW3.jpg', '42224B052-1-LW4.jpg', '42224B053-1-LW1.jpg', '42224B054-1-LW3.jpg', '42224B083-1-LW4.jpg', '45234A001-1-LW2.jpg', '45234A003-1-LW2.jpg', '45234A003-1-LW1.jpg', '45234A003-1-LW3.jpg', '45234A003-1-LW4.jpg', '45234B020-1-LW1.jpg', '45234B020-1-LW2.jpg', '45234B023-1-LW3.jpg', '45234B027-1-LW1.jpg', '45234B027-1-LW2.jpg', '45234B031-1-LW1.jpg', '45234B033-1-LW2.jpg', '45234B045-1-LW3.jpg', '45234B047-1-LW2.jpg', '45234B050-1-LF1.jpg', '45234B051-1-LW1.jpg', '45234B058-1-LW4.jpg', '45234B068-1-LW4.jpg', '45234B085-1-LW4.jpg', '45234B101-1-LF2.jpg']
+
+Other_reason = ['44232B227_FSP07.jpg']
+
+
+
+
+import os
+import shutil
+
+
+
+def find_file(filename, search_path):
+    """
+    Search for a file in the given directory and its subdirectories.
+
+    :param filename: The name of the file to search for.
+    :param search_path: The directory to search in.
+    :return: The full path to the file if found, otherwise None.
+    """
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return os.path.join(root, filename.lower())
+    return None
+
+
+def copy_file_to_directory(file_path, destination_directory):
+    """
+    Copy a file to the specified directory.
+    :param file_path: The full path of the file to copy.
+    :param destination_directory: The directory to copy the file to.
+    """
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+    shutil.copy(file_path, destination_directory)
+    pass
+
+
+
+
+
+
+
+search_path = r"D:\Group1-Batch1_Revised_20250213"
+
+
+if __name__ == "__main__":
+
+    print("Surface_not_shown 数量:", len(Surface_not_shown))
+    for file in Surface_not_shown:
+        file_path = find_file(file, search_path)
+        if file_path:
+            # print(f"Found {file} at {file_path}")
+            copy_file_to_directory(file_path, r"D:\Group1-Batch1_Revised_20250213\Surface_not_shown")
+        else:
+            print(f"{file} not found")
+
+    print("Photo_blurry 数量:", len(Photo_blurry))
+    for file in Photo_blurry:
+        file_path = find_file(file, search_path)
+        if file_path:
+            copy_file_to_directory(file_path, r"D:\Group1-Batch1_Revised_20250213\Photo_blurry")
+        else:
+            print(f"{file} not found")
+
+    for file in Surface_not_clean:
+        file_path = find_file(file, search_path)
+        if file_path:
+            copy_file_to_directory(file_path, r"D:\Group1-Batch1_Revised_20250213\Surface_not_clean")
+        else:
+            print(f"{file} not found")
+    print("Surface_not_clean 数量:", len(Surface_not_clean))
+
+    print("Other_reason 数量:", len(Other_reason))
+    for file in Other_reason:
+        file_path = find_file(file, search_path)
+        if file_path:
+            copy_file_to_directory(file_path, r"D:\Group1-Batch1_Revised_20250213\Other_reason")
+        else:
+            print(f"{file} not found in {search_path}")
+
+
+
+
+
+
+
